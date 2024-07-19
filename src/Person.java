f77c09e5a623bcf5166757354f08294a8522444d
Pathum <thilinapathumrathnayaka@gmail.com>
2024-07-19 22:51:43 +0530

    class has been Updated

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 
 class Person implements Serializable {
-    private static final long serialVersionUID = 1L;
+
     private String name;
     private int age;
 
--- src/SerializationExample.java
+++ src/SerializationExample.java
@@ -3,7 +3,7 @@ import java.io.ObjectOutputStream;
 
 public class SerializationExample {
     public static void main(String[] args) {
-        Person person = new Person("Alice", 30);
+        Person person = new Person("Pathum",23);
 
         try (FileOutputStream fileOut = new FileOutputStream("person.ser");
              ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
